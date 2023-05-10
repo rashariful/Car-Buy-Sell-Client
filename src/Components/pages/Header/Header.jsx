93de6ff653c33589fa5cc9f053a550b6392f2aa5
unknown <ra.mdsharifulislam@gmail.com>
2023-05-11 03:42:49 +0600

    new commit

--- src/Components/pages/Header/Header.jsx
+++ src/Components/pages/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
   );
 
   return (
-    <div className="navbar sticky top-0 z-50 shadow-md flex justify-between drop-shadow-sm bg-gray-100 border border-b mx-auto">
+    <div className="navbar sticky top-0 z-50 shadow-md flex justify-between drop-shadow-sm bg-white border border-b mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
